0b624c4bfd35ee0b2089f29978ce2c49738fd089
Samuele De Tomasi <el3um4s@gmail.com>
2022-09-09 23:19:44 +0200

    Update renderer.ts

--- src/IPC/renderer.ts
+++ src/IPC/renderer.ts
@@ -3,25 +3,25 @@ import { NameAPI, DefaultApiKey } from "./interfaces";
 const nameAPI: NameAPI = "windowControls";
 const defaultApiKey: DefaultApiKey = "ipc";
 
-const minimize = (options: { apiKey?: string }): void => {
+const minimize = (options?: { apiKey?: string }): void => {
   const apiKey = options?.apiKey || defaultApiKey;
   const api = globalThis[apiKey as keyof typeof globalThis][nameAPI];
   api.send("minimize", null);
 };
 
-const maximize = (options: { apiKey?: string }): void => {
+const maximize = (options?: { apiKey?: string }): void => {
   const apiKey = options?.apiKey || defaultApiKey;
   const api = globalThis[apiKey as keyof typeof globalThis][nameAPI];
   api.send("maximize", null);
 };
 
-const close = (options: { apiKey?: string }): void => {
+const close = (options?: { apiKey?: string }): void => {
   const apiKey = options?.apiKey || defaultApiKey;
   const api = globalThis[apiKey as keyof typeof globalThis][nameAPI];
   api.send("close", null);
 };
 
-const unmaximize = (options: { apiKey?: string }): void => {
+const unmaximize = (options?: { apiKey?: string }): void => {
   const apiKey = options?.apiKey || defaultApiKey;
   const api = globalThis[apiKey as keyof typeof globalThis][nameAPI];
   api.send("unmaximize", null);
